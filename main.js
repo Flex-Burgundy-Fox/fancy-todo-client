e577975b610e7cb6901e7855043a4825ab7767c1
tohajasper <tohajasper@gmail.com>
2021-07-27 22:13:42 +0700

    Login Done

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
-    console.log('hello')
+    $("#loginForm").submit(login);
 });
+
+function login(e) { 
+    e.preventDefault();
+    let data  = {
+        email : $("#inputEmail").val(),
+        password : $("#inputPassword").val()
+    }
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/login",
+        data
+    })
+    .done(({access_token}) => {
+        localStorage.setItem('access_token',access_token)
+    }).fail(err => console.log(err))
+}
